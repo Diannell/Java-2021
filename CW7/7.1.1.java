62065794b17fa0bf96a846f388c7b84af13d2e6c
Diannell <77590010+Diannell@users.noreply.github.com>
2021-12-08 19:56:03 +0200

    Add files via upload

---
+++ CW7/7.1.1.java
@@ -0,0 +1,7 @@
+import classes.First_Simple;
+
+public class CW7 {
+    public static void main(String[] args) {
+        First_Simple first_simple = new First_Simple();
+    }
+}
---
+++ CW7/7.1.4.java
@@ -0,0 +1,7 @@
+import classes.Third_Private;
+
+public class CW7 {
+    public static void main(String[] args) {
+        //Third_Private third_private = new Third_Private(); //private class won't work outside of package
+    }
+}
