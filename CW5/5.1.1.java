6fdbf9a542f7b9a5e9b76a944272ede7938ff399
Diannell <77590010+Diannell@users.noreply.github.com>
2021-12-08 20:06:18 +0200

    Delete CW5 directory

--- CW5/5.1.1.java
+++
@@ -1,8 +0,0 @@
-public class CW5 {
-    public static void main(String[] args) {
-        String[] str_array = {"a", "b", "c"};
-        for(String str : str_array) {
-            System.out.println(str);
-        }
-    }
-}
