298fcaf8f453255efb959ce7fb825218e6773d79
Diannell <77590010+Diannell@users.noreply.github.com>
2021-12-08 20:06:02 +0200

    Delete CW3 directory

--- CW3/2.3.java
+++
@@ -1,9 +0,0 @@
-public class CW3 {
-    public static String chartobin(char a){
-        return Integer.toBinaryString(a);
-    }
-    public static void main(String[] args) {
-        char a = 'a';
-        System.out.println(chartobin(a));
-    }
-}
