5cb48b7eece94d4766f5f8bf5ff0f4b6c76a2470
Diannell <77590010+Diannell@users.noreply.github.com>
2021-12-08 20:20:07 +0200

    Add files via upload

---
+++ CW1/1.1.java
@@ -0,0 +1,14 @@
+public class CW1{
+    public static void main(String[] args) {
+        HelloClass cl = new HelloClass();
+        System.out.println(cl.a);
+        System.out.println(cl.c);
+        System.out.println(cl.s);
+    }
+}
+
+class HelloClass {
+    int a;
+    char c;
+    String s;
+}
