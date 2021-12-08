d7186ccc7f5b1f039640e424841f5561bf461a79
Diannell <77590010+Diannell@users.noreply.github.com>
2021-12-08 20:07:47 +0200

    Delete CW9 directory

--- CW9/9.5.1/9.5.1.java
+++
@@ -1,9 +0,0 @@
-package cw9_5_1;
-public class CW9 {
-    public static void main(String[] args) {
-        Text text = new Text(new String[]{"Hello my friend. ", "My name is Tima. ", "What is your name? "});
-        text.set_header(new Sentence("header of this Text"));
-        text.print_header();
-        System.out.println(text);
-    }
-}
