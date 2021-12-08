b1176c39e450ac811a38755dcc2f106e1c0267ea
Diannell <77590010+Diannell@users.noreply.github.com>
2021-12-08 20:04:44 +0200

    Delete CW1 directory

--- CW1/1.6.java
+++
@@ -1,9 +0,0 @@
-public class CW1 {
-    public static void main(String[] args) {
-        double sum = 0;
-        for (int i = 0; i < args.length; i++) {
-            sum += Double.valueOf(args[i]);
-        }
-        System.out.println("sum=" + sum);
-    }
-}
