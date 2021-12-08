b8a38156150442ed83e44ac9753fa69e7b67604d
Diannell <77590010+Diannell@users.noreply.github.com>
2021-12-08 20:07:15 +0200

    Delete CW6 directory

--- CW6/6.2.1.java
+++
@@ -1,8 +0,0 @@
-public class CW6 {
-    public static void main(String[] args) {
-        Matrix matrix = new Matrix();
-        matrix.matrix_random();
-        matrix.print();
-        matrix.sort_k("column", 2);
-    }
-}
