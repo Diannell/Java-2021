0567607113c1b143a1ff60e0a946daec419c22b3
Diannell <77590010+Diannell@users.noreply.github.com>
2021-12-08 20:08:20 +0200

    Delete HW2 directory

--- HW2/3.6.java
+++
@@ -1,8 +0,0 @@
-public class HW2 {
-    public static void main(String[] args) {
-        String s1 = "Hello";
-        String s2 = "World!";
-        System.out.println("!!!" + s1 + "," + " "+ s2 + "!!!");
-    }
-}
-//
